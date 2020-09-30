fffb71f9ee89ec8930ea166c955edbaf3d4b6366
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-09-30 21:33:45 +0530

    initial commit

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    googleClientID     :  process.env.googleClientID,
-    googleClientSecret :  process.env.googleClientSecret,
-    mongoURI           :  process.env.mongoURI,
-    cookieKey          :  process.env.cookieKey, 
-}
